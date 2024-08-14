a3183f9adcf8993f910dd680852fbac8c1fb6ec7
fdw <fddewaard@gmail.com>
2024-08-14 21:43:07 +0200

    day1 part1

---
+++ _2016/src/day1/Main.java
@@ -0,0 +1,19 @@
+package day1;
+
+import java.util.List;
+
+import utils.InputParser;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> inputs = new InputParser("_2016/src/day1/input.txt").getLinesSplitByComma();
+        Mover mover = new Mover();
+        inputs.forEach(input -> {
+            int steps = Integer.parseInt(input.substring(1));
+            mover.changeDirection(input.split("")[0]);
+            mover.move(steps);
+        });
+        System.out.println("Distance travelled: " + Math.abs(-mover.getX() + Math.abs(-mover.getY())));
+    }
+
+}
