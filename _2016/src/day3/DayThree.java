f2d42e218452699bb5a5ae4e7aaa2d97ed8a34b1
Frank de Waard <frank.dewaard@duo.nl>
2024-08-15 13:07:21 +0200

    onelegante oplossing even snel tijdens de pauze

--- _2016/src/day3/DayThree.java
+++ _2016/src/day3/DayThree.java
@@ -10,12 +10,21 @@ public static void main(String[] args) {
     List<String> inputs = new InputParser("_2016/src/day3/input.txt").getLines();
     TriangleHandler triangleHandler = new TriangleHandler();
     List<String> possibleTriangles = new ArrayList<>();
+    List<List<String>> possibleTriangles2 = new ArrayList<>();
+    List<List<String>> trianglesPartTwo = new ArrayList<>();
     inputs.forEach(input -> {
         List<String> triangle = InputParser.splitBy(input, "\\s+");
+        trianglesPartTwo.add(triangle);
         if (triangleHandler.calculateTrianglePossible(triangle)) {
             possibleTriangles.add(input);
         }
     });
     System.out.println("Amount of possible triangles: " + possibleTriangles.size());
+    triangleHandler.getVerticalTriangles(trianglesPartTwo).forEach(triangle -> {
+        if (triangleHandler.calculateTrianglePossible(triangle)) {
+            possibleTriangles2.add(triangle);
+        }
+    });
+    System.out.println("Amount of possible triangles for part two: " + possibleTriangles2.size());
 }
 }
--- _2016/src/day3/TriangleHandler.java
+++ _2016/src/day3/TriangleHandler.java
@@ -1,14 +1,36 @@
 package day3;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class TriangleHandler {
 
+    List<List<String>> trianglesCol1 = new ArrayList<>();
+    List<List<String>> trianglesCol2 = new ArrayList<>();
+    List<List<String>> trianglesCol3 = new ArrayList<>();
+
     public boolean calculateTrianglePossible(List<String> triangle) {
-        int[] sides = {Integer.parseInt(triangle.getFirst()), Integer.parseInt(triangle.get(1)), Integer.parseInt(triangle.getLast())};
+        int[] sides = { Integer.parseInt(triangle.getFirst()), Integer.parseInt(triangle.get(1)), Integer.parseInt(triangle.getLast()) };
         sides = Arrays.stream(sides).sorted().toArray();
         return sides[0] + sides[1] > sides[2];
+    }
+
+    public List<List<String>> getVerticalTriangles(List<List<String>> inputs) {
+        for (int i = 0; i < inputs.size(); i++) {
+            if (i % 3 == 0) {
+                trianglesCol1.add(new ArrayList<>());
+                trianglesCol2.add(new ArrayList<>());
+                trianglesCol3.add(new ArrayList<>());
+            }
+            int index = Math.floorDiv(i, 3);
+            trianglesCol1.get(index).add(inputs.get(i).get(0));
+            trianglesCol2.get(index).add(inputs.get(i).get(1));
+            trianglesCol3.get(index).add(inputs.get(i).get(2));
+        }
+        trianglesCol1.addAll(trianglesCol2);
+        trianglesCol1.addAll(trianglesCol3);
+        return trianglesCol1;
     }
 
 }
