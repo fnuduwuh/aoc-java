03b5cd2274cbafad2d932fe6ef1d86ef24a3aa48
fdw <fddewaard@gmail.com>
2024-08-17 09:39:08 +0200

    opmaak

--- _2016/src/day5/DayFive.java
+++ _2016/src/day5/DayFive.java
@@ -24,14 +24,13 @@ public class DayFive {
             String door = doorID + addTo;
             byte[] digest = MessageDigest.getInstance("MD5").digest(door.getBytes());
             String hexString = toHex(digest);
-//            System.out.println("Hashes processed: " + addTo);
             if (hexString.startsWith("00000")) {
                 int passwordIndex = StringHelper.isNumeric(String.valueOf(hexString.charAt(5))) ? Integer.parseInt(String.valueOf(hexString.charAt(5))) : 9;
                 System.out.println("Possible password digit! Processing: " + hexString);
                 if (passwordIndex < 8 && password2[passwordIndex] == null) {
                     passwordLength++;
                     password2[passwordIndex] = String.valueOf(hexString.charAt(6));
-                    System.out.println("Password digit found! \n Building password: " + Arrays.toString(password2));
+                    System.out.println("Password digit found! \nBuilding password: " + Arrays.toString(password2));
                 }
                 if (passwordLength == 8) {
                     System.out.println("Password found: " + Arrays.toString(password2));
@@ -50,7 +49,6 @@ public class DayFive {
             String door = doorID + addTo;
             byte[] digest = MessageDigest.getInstance("MD5").digest(door.getBytes());
             String hexString = toHex(digest);
-//            System.out.println("Hashes processed: " + addTo);
             if (hexString.startsWith("00000")) {
                 System.out.println("Password digit found: " + hexString.charAt(5));
                 password.append(hexString.charAt(5));
