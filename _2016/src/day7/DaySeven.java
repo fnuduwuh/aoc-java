8bb3f0adc80d3a2077234d3c8d45e0db2ccb6fbe
fdw <fddewaard@gmail.com>
2024-08-23 19:40:02 +0200

    pt2

--- _2016/src/day7/DaySeven.java
+++ _2016/src/day7/DaySeven.java
@@ -9,13 +9,19 @@ public class DaySeven {
     public static void main(String[] args) {
         List<String> lines = new InputParser("_2016/src/day7/input.txt").getLines();
         AtomicInteger count = new AtomicInteger();
+        AtomicInteger count2 = new AtomicInteger();
         NetworkAnalyzer networkAnalyzer = new NetworkAnalyzer();
 
         lines.forEach(line -> {
             if (networkAnalyzer.checkTLS(line)) {
                 count.getAndIncrement();
-                System.out.println("line is valid " + count);
+            }
+            if (networkAnalyzer.checkSSL(line)) {
+                count2.getAndIncrement();
             }
         });
+        System.out.println("IP supporting TLS count: " + count);
+        System.out.println("IP supporting SSL count: " + count2);
+
     }
 }
--- _2016/src/day7/NetworkAnalyzer.java
+++ _2016/src/day7/NetworkAnalyzer.java
@@ -1,6 +1,35 @@
 package day7;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NetworkAnalyzer {
+
+    public boolean checkSSL(String IP) {
+        boolean hypernet = false;
+        List<String> aba = new ArrayList<>();
+        List<String> bab = new ArrayList<>();
+
+        String[] split = IP.split("");
+        for (int i = 0; i < split.length - 2; i++) {
+            if (split[i].equals("[")) {
+                hypernet = true;
+            } else if (split[i].equals("]")) {
+                hypernet = false;
+            } else if (split[i].equals(split[i + 2])) {
+                if (hypernet) {
+                    String mirrored = split[i + 1] + split[i] + split[i + 1];
+                    bab.add(mirrored);
+                    if (aba.contains(mirrored)) return true;
+                } else {
+                    String hit = split[i] + split[i + 1] + split[i + 2];
+                    aba.add(hit);
+                    if (bab.contains(hit)) return true;
+                }
+            }
+        }
+        return false;
+    }
 
     public boolean checkTLS(String IP) {
         boolean hypernet = false;
